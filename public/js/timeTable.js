446c2c0cbab33b39f83e3804050eaff6a224c812
LaffeyZaychik <34617016+LaffeyTaffey@users.noreply.github.com>
2024-04-16 15:00:13 +0800

    updated my scheduke

---
+++ public/js/timeTable.js
@@ -0,0 +1,133 @@
+const Sunday =[
+    {   
+        time: 'Sunday',
+        roomNumber: 'Holiday',
+        subject: 'No class Available',
+        type: 'walang pasok woy'
+    }
+]
+const Monday =[
+    {   
+        time: '07-11 AM',
+        roomNumber: 'RM-405',
+        subject: 'SAD300',
+        type: 'Lecture'
+    },
+    {   
+        time: '01-3 PM',
+        roomNumber: 'RM-311',
+        subject: 'DMTH301',
+        type: 'Quiz'
+    },
+    {   
+        time: '01-05 PM',
+        roomNumber: 'RM-310',
+        subject: 'MG302',
+        type: 'Lecture'
+    }
+]
+const Tuesday =[
+    {   
+        time: '09-10 AM',
+        roomNumber: 'RM-310',
+        subject: 'DMTH301',
+        type: 'Tutorial'
+    },
+    {   
+        time: '11-12 AM',
+        roomNumber: 'RM-310',
+        subject: 'SAD300',
+        type: 'Lecture'
+    },
+    {   
+        time: '12-01 PM',
+        roomNumber: 'RM-310',
+        subject: 'DMTH301',
+        type: 'Lecture'
+    },
+    {   
+        time: '02-03 PM',
+        roomNumber: 'RM-405',
+        subject: 'SAD300',
+        type: 'Lecture'
+    }
+]
+
+const Wednesday =[
+    {   
+        time: '10-11 AM',
+        roomNumber: 'RM-405',
+        subject: 'OR305',
+        type: 'Lecture'
+    },
+    {   
+        time: '11-12 AM',
+        roomNumber: 'RM-315',
+        subject: 'DMTH302',
+        type: 'Lecture'
+    }
+]
+
+const Thursday =[
+    {   
+        time: '11-12 AM',
+        roomNumber: 'RM-415',
+        subject: 'MTH16',
+        type: 'Lecture'
+    },
+    {   
+        time: '01-02 PM',
+        roomNumber: 'RM-134',
+        subject: 'DE2301',
+        type: 'Lecture'
+    },
+    {   
+        time: '02-03 PM',
+        roomNumber: 'RM-123',
+        subject: 'OR200',
+        type: 'Lecture'
+    }
+]
+
+const Friday =[
+    {   
+        time: '10-11 AM',
+        roomNumber: 'RM-305',
+        subject: 'MG175',
+        type: 'Lecture'
+    },
+    {   
+        time: '11-12 AM',
+        roomNumber: 'RM-981',
+        subject: 'OR',
+        type: 'Lecture'
+    },
+    {   
+        time: '02-03 PM',
+        roomNumber: 'RM-761',
+        subject: 'DE21',
+        type: 'Tutorial'
+    },
+
+]
+
+const Saturday =[
+    {   
+        time: '09-10 AM',
+        roomNumber: 'RM-102',
+        subject: 'DMTH194',
+        type: 'Lesson'
+    },
+    {   
+        time: '10-11 AM',
+        roomNumber: 'RM-102',
+        subject: 'SAD1',
+        type: 'Lecture'
+    },
+    {   
+        time: '01-02 PM',
+        roomNumber: 'RM-405',
+        subject: 'OR104',
+        type: 'Lecture'
+    }
+]
