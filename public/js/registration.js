4329dc17e7bb672ea9e825027a8469c38c3a9848
raicem-caelia <itscicil72469@gmail.com>
2024-04-30 20:24:04 +0800

    backend nalang

--- public/js/registration.js
+++ public/js/registration.js
@@ -3,7 +3,7 @@ showTab(currentTab); // Display the current tab
 
 function showTab(n) {
     // This function will display the specified tab of the form ...
-    var x = document.getElementsByClassName("registrationForms");
+    var x = document.getElementsByClassName("tab");
     x[n].style.display = "block";
     // ... and fix the Previous/Next buttons:
     if (n == 0) {
@@ -22,7 +22,7 @@ function showTab(n) {
 
   function nextPrev(n) {
     // This function will figure out which tab to display
-    var x = document.getElementsByClassName("registrationForms");
+    var x = document.getElementsByClassName("tab");
     // Exit the function if any field in the current tab is invalid:
     if (n == 1 && !validateForm()) return false;
     // Hide the current tab:
@@ -33,7 +33,7 @@ function showTab(n) {
     if (currentTab >= x.length) {
       //...the form gets submitted:
       document.getElementById("reg").submit();
-      return false;
+      return true;
     }
     // Otherwise, display the correct tab:
     showTab(currentTab);
@@ -42,7 +42,7 @@ function showTab(n) {
   function validateForm() {
     // This function deals with validation of the form fields
     var x, y, i, valid = true;
-    x = document.getElementsByClassName("registrationForms");
+    x = document.getElementsByClassName("tab");
     y = x[currentTab].getElementsByTagName("input");
     // A loop that checks every input field in the current tab:
     for (i = 0; i < y.length; i++) {
@@ -70,3 +70,7 @@ function showTab(n) {
     //... and adds the "active" class to the current step:
     x[n].className += " active";
   }
+
+
+
+  
